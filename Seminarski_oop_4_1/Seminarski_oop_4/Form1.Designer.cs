d6e741d506d10be92aff0262e63bd0da9727951b
mzdero00 <mzdero00@fesb.hr>
2022-01-17 02:52:25 +0100

    marin

---
+++ Seminarski_oop_4_1/Seminarski_oop_4/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿
+namespace Seminarski_oop_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.startButton = new System.Windows.Forms.Button();
+            this.snapButton = new System.Windows.Forms.Button();
+            this.picCanvas = new System.Windows.Forms.PictureBox();
+            this.txtScore = new System.Windows.Forms.Label();
+            this.txtHighScore = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picCanvas)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.BackColor = System.Drawing.Color.LightGreen;
+            this.startButton.Font = new System.Drawing.Font("Poor Richard", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(31, 11);
+            this.startButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(90, 46);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.StartGame);
+            // 
+            // snapButton
+            // 
+            this.snapButton.Font = new System.Drawing.Font("Poor Richard", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.snapButton.Location = new System.Drawing.Point(31, 62);
+            this.snapButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.snapButton.Name = "snapButton";
+            this.snapButton.Size = new System.Drawing.Size(90, 51);
+            this.snapButton.TabIndex = 0;
+            this.snapButton.Text = "Screenshot";
+            this.snapButton.UseVisualStyleBackColor = true;
+            this.snapButton.Click += new System.EventHandler(this.TakeSnapShot);
+            // 
+            // picCanvas
+            // 
+            this.picCanvas.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.picCanvas.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("picCanvas.BackgroundImage")));
+            this.picCanvas.Location = new System.Drawing.Point(151, 1);
+            this.picCanvas.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.picCanvas.Name = "picCanvas";
+            this.picCanvas.Size = new System.Drawing.Size(751, 499);
+            this.picCanvas.TabIndex = 1;
+            this.picCanvas.TabStop = false;
+            this.picCanvas.Paint += new System.Windows.Forms.PaintEventHandler(this.UpadatePicutreBoxGraphics);
+            // 
+            // txtScore
+            // 
+            this.txtScore.AutoSize = true;
+            this.txtScore.Font = new System.Drawing.Font("Poor Richard", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtScore.Location = new System.Drawing.Point(47, 144);
+            this.txtScore.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(49, 16);
+            this.txtScore.TabIndex = 2;
+            this.txtScore.Text = "Score :  0";
+            // 
+            // txtHighScore
+            // 
+            this.txtHighScore.AutoSize = true;
+            this.txtHighScore.Font = new System.Drawing.Font("Poor Richard", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHighScore.Location = new System.Drawing.Point(47, 173);
+            this.txtHighScore.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.txtHighScore.Name = "txtHighScore";
+            this.txtHighScore.Size = new System.Drawing.Size(64, 16);
+            this.txtHighScore.TabIndex = 2;
+            this.txtHighScore.Text = "High score:  ";
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 40;
+            this.gameTimer.Tick += new System.EventHandler(this.GameTimerEvent);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pictureBox1.Location = new System.Drawing.Point(-6, 192);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(160, 308);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(904, 501);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtHighScore);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.picCanvas);
+            this.Controls.Add(this.snapButton);
+            this.Controls.Add(this.startButton);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Snake";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.picCanvas)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button snapButton;
+        private System.Windows.Forms.PictureBox picCanvas;
+        private System.Windows.Forms.Label txtScore;
+        private System.Windows.Forms.Label txtHighScore;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
